d3511393f5590cc5e1f4a34f7a818c05d28b3fcc
Xiaocao <wdjoys@hotmail.com>
2021-01-20 11:34:23 +0800

    代码格式化样式配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ['plugin:vue/essential', '@vue/standard'],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        // 强制使用单引号
+        quotes: ['error', 'single'],
+        // 强制不使用分号结尾
+        semi: ['error', 'never'],
+        'no-unused-vars': 'off',
+        indent: ['error', 4]
+    }
 }
