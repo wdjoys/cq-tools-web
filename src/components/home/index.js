a4ae9d7c280d6c72bd9dc0a5ca4ea7e446d67d59
Xiaocao <wdjoys@hotmail.com>
2021-03-16 17:52:59 +0800

    echart g2plot chartsjs 尝试

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,5 +1,6 @@
 import service from './service'
 import monitor from './monitor'
 import slideshow from './slideshow'
+import chartjs from './chartjs'
 
-export { service, monitor, slideshow }
+export { service, monitor, slideshow, chartjs }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,25 +7,24 @@
             <!-- <a-col :span="24">
                 <service class="service-status" />
             </a-col> -->
-            <a-col :span="12"
-                   style="height:800px">
+            <a-col :span="12">
                 <monitor class="service-status" />
             </a-col>
-            <!-- <a-col :span="12"
-                   style="height:500px">
-                <service class="service-status" />
-            </a-col> -->
+            <a-col :span="12">
+                <chartjs class="service-status" />
+            </a-col>
         </a-row>
     </div>
 </template>
 
 <script>
-import { service, monitor, slideshow } from '@/components/home'
+import { service, monitor, slideshow, chartjs } from '@/components/home'
 export default {
     components: {
         // service,
         monitor,
-        slideshow
+        slideshow,
+        chartjs
     }
 
 }
