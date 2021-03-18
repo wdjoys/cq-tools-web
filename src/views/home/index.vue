c8465323153949973a05da8fe283559543216725
Xiaocao <wdjoys@hotmail.com>
2021-03-18 17:39:38 +0800

    0316

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,24 +7,75 @@
             <!-- <a-col :span="24">
                 <service class="service-status" />
             </a-col> -->
-            <a-col :span="12">
-                <monitor class="service-status" />
+            <a-col :span="12"
+                   class="service-status">
+
+                <load :load="serverInfo.load" />
             </a-col>
-            <a-col :span="12">
-                <chartjs class="service-status" />
+            <a-col :span="12"
+                   class="service-status">
+
+                <network :network="serverInfo.network" />
             </a-col>
         </a-row>
     </div>
 </template>
 
 <script>
-import { service, monitor, slideshow, chartjs } from '@/components/home'
+import { service, network, slideshow, load } from '@/components/home'
 export default {
     components: {
         // service,
-        monitor,
+        load,
         slideshow,
-        chartjs
+        network
+    },
+    data () {
+        return {
+            serverInfo: {
+                load: {
+                    cpu: [],
+                    ram: [],
+                    disk: []
+
+                },
+                network: {
+                    in: [],
+                    out: []
+                }
+            }
+        }
+    },
+    methods: {
+        data_generater (i = 0) {
+            // 时间戳
+            const timeStamp = new Date().getTime() - i * 5000
+
+            this.in = this.load.cpu[-1] + Math.round(Math.random() * 100) - 50
+            this.out = this.out + Math.round(Math.random() * 100) - 50
+            return {
+                x: timeStamp,
+                y: this.out
+            }
+        },
+        // 构造初始数据
+        data_list_generater () {
+            const data = []
+            for (let i = 0; i < 120; i++) {
+                data.push(this.data_generater(i))
+            }
+            return data
+        }
+
+    },
+    mounted () {
+        setInterval(() => {
+            for (const key in this.serverInfo.network) {
+                if (Object.hasOwnProperty.call(object, key)) {
+                    const element = object[key]
+                }
+            }
+        }, 3000)
     }
 
 }
@@ -33,7 +84,13 @@ export default {
 <style lang="less" scoped>
 .home {
     .service-status {
-        background-color: rgb(255, 255, 255);
+        // background-color: rgb(255, 255, 255);
+        height: 430px;
+
+        div {
+            background-color: rgb(255, 255, 255);
+            height: 100%;
+        }
     }
 }
 </style>
