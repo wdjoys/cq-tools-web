b3a68938cccf7e0384964bfaf1de713e0e7321f9
Xiaocao <wdjoys@hotmail.com>
2021-03-18 17:40:14 +0800

    0316

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,8 @@
 import service from './service'
 import monitor from './monitor'
 import slideshow from './slideshow'
-import chartjs from './chartjs'
 
-export { service, monitor, slideshow, chartjs }
+import network from './chart/network'
+import load from './chart/load'
+
+export { service, monitor, slideshow, network, load }
