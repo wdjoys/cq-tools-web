564b8ca8854ee5aaf7193d24d154331af72ba43c
xiaocao <wdjoys@hotmail.com>
2021-05-04 01:37:52 +0800

    web socket 使用加密协议

--- src/api/socket/socket.js
+++ src/api/socket/socket.js
@@ -1,7 +1,8 @@
 import store from '@/store'
+import { serviceURL } from '@/config/config.common'
 class ServerLoad {
     constructor (wsUrl, receviFunc) {
-        this.ws = new WebSocket(`ws://${store.getters.get_server.ip}:7890/${wsUrl}`)
+        this.ws = new WebSocket(`wss://${store.getters.get_server.id.toString().padStart(6, '0')}.${serviceURL}/${wsUrl}`)
         this.ws.onmessage = (event) => { receviFunc(this.receviMessage(event)) }
 
         // this.ws.onmessage = this.websocketonmessage;
