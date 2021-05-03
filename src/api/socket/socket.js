19717a3953642c5051f148448b8ef119e88364a2
xiaocao <wdjoys@hotmail.com>
2021-05-04 01:43:18 +0800

    1

--- src/api/socket/socket.js
+++ src/api/socket/socket.js
@@ -2,7 +2,7 @@ import store from '@/store'
 import { serviceURL } from '@/config/config.common'
 class ServerLoad {
     constructor (wsUrl, receviFunc) {
-        this.ws = new WebSocket(`wss://${store.getters.get_server.id.toString().padStart(6, '0')}.${serviceURL}/${wsUrl}`)
+        this.ws = new WebSocket(`wss://${store.getters.get_server.id.toString().padStart(6, '0')}.${serviceURL}${wsUrl}`)
         this.ws.onmessage = (event) => { receviFunc(this.receviMessage(event)) }
 
         // this.ws.onmessage = this.websocketonmessage;
